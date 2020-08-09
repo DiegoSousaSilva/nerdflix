47b846614335b5ef27ccaf6d8252b8d7dca98d3d
Diego Sousa <derickbenji2@gmail.com>
2020-08-09 19:14:04 -0300

    commit 9 /ago 19:15

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/img/nerdflix.png';
 import './menu.css';
 //import ButtonLink from './components/ButtonLink';
@@ -10,10 +10,10 @@ const Menu = () =>{
 
   return(
     <nav className='menu'>
-      <a href="/">
+      <Link to="/">
         <img className='logo' src={Logo} alt="logo"/>
-      </a>
-      <Button className='buttonLink' href="/"> 
+      </Link>
+      <Button as={Link} className='buttonLink' to="cadastro/video"> 
         Novo Video
       </Button   >
     </nav>
