bff412afe919c2de0fbd28aa64c289ba911ca158
Diego Sousa <derickbenji2@gmail.com>
2020-09-13 00:16:21 -0300

    refazendo db

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,7 @@ import PageDefault from '../../../components/PageDefault';
 // eslint-disable-next-line import/no-named-as-default
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 const CadastroCategoria = () => {
   const valoresIniciais = useState([
@@ -17,22 +18,8 @@ const CadastroCategoria = () => {
     },
   ]);
 
-  const [values, setValues] = useState(valoresIniciais);
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
   const [categorias, setCategorias] = useState([]);
-
-  const setValue = (key, value) => {
-    setValues({
-      ...values,
-      [key]: value,
-    });
-  };
-
-  function handleChange(info) {
-    setValue(
-      info.target.getAttribute('name'),
-      info.target.value,
-    );
-  }
 
   useEffect(() => {
     const URL = window.location.hostname.includes('localhost')
@@ -61,7 +48,7 @@ const CadastroCategoria = () => {
           ...categorias,
           values,
         ]);
-        setValues(valoresIniciais);
+        clearForm();
       }}
       >
 
@@ -101,8 +88,8 @@ const CadastroCategoria = () => {
 
       <ul>
         {categorias.map((categorias) => (
-          <li key={`${categorias}${categorias.nome}`}>
-            {categorias.nome}
+          <li key={`${categorias}${categorias.titulo}`}>
+            {categorias.titulo}
 
           </li>
         ))}
---
+++ src/repositories/categorias.js
@@ -0,0 +1,19 @@
+import config from '../config';
+
+const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`;
+
+function getAllWithVideos() {
+  return fetch(`${URL_CATEGORIES}/?_embed=videos`)
+    .then(async (resServer) => {
+      if (resServer.ok); {
+        const res = await resServer.json();
+        return res;
+      }
+      // eslint-disable-next-line no-unreachable
+      throw new Error('Não foi possivel acessar aos dados :(');
+    });
+}
+
+export default {
+  getAllWithVideos,
+};
